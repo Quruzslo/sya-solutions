85a9f715a4b0c0338948b10de4812a126ac5e4eb
Gál Áron Szili <sziligalaron@gmail.com>
2026-07-01 15:13:37 +0200

    titles reszponziv

--- src/app/components/sectionTitles.jsx
+++ src/app/components/sectionTitles.jsx
@@ -1,8 +1,8 @@
 export default function SectionTitles({ title, bgText }) {
   return (
     <div className="flex flex-col relative w-full">
-      <h2 className="!text-[30px] text-sotet font-black">{title}</h2>
-      <p className="absolute flex top-[-50px] left-0 text-zold font-bold text-[35px] md:text-[60px] xl:text-[100px]  opacity-10">
+      <h2 className="!text-[20px] !md:text-[30px] text-sotet font-black">{title}</h2>
+      <p className="absolute flex top-[-25px] md:top-[-50px] left-0 text-zold font-bold text-[25px] md:text-[60px] xl:text-[100px]  opacity-10">
         {bgText}
       </p>
     </div>
