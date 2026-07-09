1da3f9452dd8bdb5dc78703e5ce75b8efc110c93
Gál Áron Szili <sziligalaron@gmail.com>
2026-07-09 17:36:28 +0200

    interfaciális megoldások

---
+++ next-auth.d.ts
@@ -0,0 +1,30 @@
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+
+declare module "next-auth" {
+  /**
+   * A 'user' objektum kiterjesztése
+   */
+  interface User extends DefaultUser {
+    role?: string;
+  }
+
+  /**
+   * A 'session' objektum kiterjesztése
+   */
+  interface Session {
+    user: {
+      id?: string;
+      role?: string;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  /**
+   * A 'jwt' (token) objektum kiterjesztése
+   */
+  interface JWT {
+    userId?: string | null;
+    role?: string;
+  }
+}
