a67c55120a4c9a5175131f77d29fbe5c4dd1e27c
Gál Áron Szili <sziligalaron@gmail.com>
2026-07-09 17:33:52 +0200

    Machinációk

--- app/admin-belepes/fiok/page.jsx
+++ app/admin-belepes/fiok/page.jsx
@@ -11,7 +11,7 @@ export default async function FiokPage() {
   return (
     <section className="w-full min-h-screen flex flex-col pt-[120px]">
       <div className="w-full flex-1 flex flex-col md:flex-row">
-        <div className="w-full !h-full flex flex-col md:w-[300px]  p-[10px] items-center">
+        <div className="w-full  flex flex-col md:w-[300px]  p-[10px] items-center">
           <AdminNavComp></AdminNavComp>
         </div>
         <div className="w-full flex flex-col md:w-[100%]  p-[10px]">
