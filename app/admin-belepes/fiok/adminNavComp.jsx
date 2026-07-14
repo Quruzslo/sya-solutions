8aed5662a20fdd2fe3b386549594c2cdf8051507
Gál Áron Szili <sziligalaron@gmail.com>
2026-07-14 19:46:47 +0200

    shadow alapból levéve

--- app/admin-belepes/fiok/adminNavComp.jsx
+++ app/admin-belepes/fiok/adminNavComp.jsx
@@ -5,14 +5,14 @@ import SignOutButton from "./SignOutButton";
 
 export default function AdminNavComp() {
   return (
-    <div className="flex flex-col gap-3 w-full h-full">
+    <div className="flex flex-col gap-4 w-full h-full">
       <UserPanel />
 
       {navItem.map((item, ind) => (
         <Link
           key={ind}
           href={item.path}
-          className="flex flex-row gap-3 cursor-pointer hover:opacity-80 bg-zold/10 w-full p-[5px] items-center justify-center rounded-full shadow-[2px_2px_10px_1px_rgba(0,0,0,0.1)] hover:shadow-[2px_2px_10px_1px_rgba(0,0,0,0.6)] text-slate-700 hover:text-slate-900"
+          className="flex flex-row gap-3 cursor-pointer hover:opacity-80 bg-zold/10 w-full p-[5px] items-center justify-center rounded-full shadow-[2px_2px_10px_1px_rgba(0,0,0,0)] hover:shadow-[2px_2px_10px_1px_rgba(0,0,0,0.6)] text-slate-700 hover:text-slate-900"
         >
           <p>{item.name}</p>
         </Link>
