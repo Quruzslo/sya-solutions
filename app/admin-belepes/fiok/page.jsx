4f498ea63f34b4047f8b41a4e60d6bf3c63f9726
Gál Áron Szili <sziligalaron@gmail.com>
2026-07-14 17:12:09 +0200

    admin panel

--- app/admin-belepes/fiok/page.jsx
+++ app/admin-belepes/fiok/page.jsx
@@ -13,7 +13,7 @@ export default async function FiokPage() {
     <section className="w-full min-h-screen flex flex-col pt-[120px]">
       <div className="w-full flex-1 flex flex-col md:flex-row">
         <div className="w-full  flex flex-col md:w-[300px]  p-[10px] items-center">
-          <AdminNavComp session={session}></AdminNavComp>
+          <AdminNavComp></AdminNavComp>
         </div>
         <div className="w-full flex flex-col md:w-[100%]  p-[10px]">
           Grafikonok és minden más
