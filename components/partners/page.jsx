5622e498cadc7d00bf66dabc6a2989652fee0f76
Gál Áron Szili <sziligalaron@gmail.com>
2026-07-16 18:27:30 +0200

    partnerek szekció

--- components/partners/page.jsx
+++ components/partners/page.jsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import { Partners } from "./partners";
 import styles from "./PartnerCarousel.module.css";
+import SectionTitles from "../sectionTitles";
 
 const PartnerCarousel = () => {
   return (
     <div className={styles.carouselContainer}>
+      <div className="mx-[5%]">
+        <SectionTitles
+          title={"Partnereink"}
+          bgText={"Akik a biztos hátteret adják"}
+        />
+      </div>
       <div className={styles.carouselTrack}>
         {[...Partners, ...Partners].map((partner, index) => (
           <div key={index} className={styles.partnerLogoWrapper}>
