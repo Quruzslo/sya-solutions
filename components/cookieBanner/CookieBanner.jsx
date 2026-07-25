c47eb32a9cd1d37ca251f5afb3f8d941515e15f9
Gál Áron Szili <sziligalaron@gmail.com>
2026-07-25 15:43:58 +0200

    süti ikonka

--- components/cookieBanner/CookieBanner.jsx
+++ components/cookieBanner/CookieBanner.jsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { TbCookieManFilled } from "react-icons/tb";
 
 export default function CookieBanner() {
   const router = useRouter();
@@ -33,11 +34,11 @@ export default function CookieBanner() {
       {!showBanner ? (
         <div
           onClick={() => setShowBanner(true)}
-          className="fixed bottom-[10px] left-[10px] h-[45px] bg-feher rounded-full group z-50 cursor-pointer  grid grid-cols-[45px_0fr] hover:grid-cols-[45px_1fr] transition-all duration-300 ease-in-out items-center overflow-hidden"
+          className="fixed bottom-[10px] left-[10px] h-[45px] bg-transparent hover:bg-feher rounded-full group z-50 cursor-pointer  grid grid-cols-[45px_0fr] hover:grid-cols-[45px_1fr] transition-all duration-300 ease-in-out items-center overflow-hidden"
         >
           {/* 1. Oszlop: A süti ikon */}
           <div className="w-[45px] h-[45px] flex items-center justify-center shrink-0">
-            <span className="text-[30px] leading-none">🍪</span>
+            <TbCookieManFilled size={45} className="text-zold/50" />
           </div>
 
           <div className="overflow-hidden">
@@ -49,8 +50,8 @@ export default function CookieBanner() {
       ) : (
         // 2. NAGY BANNER (Ha a showBanner true)
         <div className="fixed bottom-[10px] left-[10px] right-[10px] md:right-[100%] w-[95%] mx-auto md:max-w-md p-5 bg-zinc-900 text-white rounded-xl shadow-2xl  z-50 animate-in fade-in slide-in-from-bottom-5">
-          <h3 className="font-semibold !text-[20px] mb-2">
-            Süti (Cookie) beállítások 🍪
+          <h3 className="font-semibold !text-[20px] mb-2 flex flex-row nowrap items-center gap-3">
+            Süti (Cookie) beállítások <TbCookieManFilled size={20} />
           </h3>
           <p className="text-sm text-zinc-400 mb-4">
             Az oldal működéséhez szükséges sütiken kívül harmadik féltől
