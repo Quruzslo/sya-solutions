b30c8fabef145610cbec911867aa0d2d4ce177f7
Gál Áron Szili <sziligalaron@gmail.com>
2026-07-30 17:50:35 +0200

    assistens ikonka

--- app/rolunk/carrierData.js
+++ app/rolunk/carrierData.js
@@ -116,6 +116,38 @@ export const carrierData = [
   {
     id: 4,
     title: "Értékesítési Asszisztens",
+    icon: (
+      <svg
+        fill="#3f4603"
+        version="1.1"
+        id="Capa_1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        width="25px"
+        height="25px"
+        viewBox="0 0 31.361 31.361"
+        xmlSpace="preserve"
+      >
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          <g>
+            <path d="M22.277,18.415c-3.574,0-6.474,2.897-6.474,6.474c0,3.574,2.899,6.473,6.474,6.473s6.475-2.897,6.475-6.473 C28.752,21.312,25.853,18.415,22.277,18.415z M20.889,28.869l-3.457-3.456l1.357-1.358l2.1,2.1l4.568-4.568l1.356,1.357 L20.889,28.869z"></path>
+            <path d="M4.326,28.289V1.719h13.363v6.884h6.885v9.22c0.612,0.198,1.188,0.478,1.719,0.819V7.891L18.401,0H2.609v30.008h14.293 c-0.486-0.51-0.9-1.087-1.227-1.719H4.326z"></path>
+            <circle cx="8.788" cy="11.686" r="2.426"></circle>
+            <rect x="13.391" y="10.246" width="8.238" height="2.88"></rect>
+            <circle cx="8.788" cy="18.094" r="2.426"></circle>
+            <path d="M21.629,17.488v-0.834h-8.238v2.88h3.744C18.324,18.391,19.891,17.639,21.629,17.488z"></path>
+            <circle cx="8.788" cy="24.19" r="2.426"></circle>
+            <path d="M13.391,22.751v2.88h1.49c-0.024-0.245-0.038-0.492-0.038-0.742c0-0.744,0.113-1.459,0.316-2.138H13.391z"></path>
+          </g>
+        </g>
+      </svg>
+    ),
     description:
       "Pénzügyi és biztosítási területen működő cégünk értékesítési asszisztens munkatársat keres ügyfélgondozásra, telefonos kapcsolattartásra és adminisztrációs feladatok ellátására.",
     elvarasok: [
