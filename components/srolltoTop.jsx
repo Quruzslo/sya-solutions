3cd52a230ec71991783cdeceb62e51ba0761d47d
Gál Áron Szili <sziligalaron@gmail.com>
2026-07-31 11:31:28 +0200

    scroll to top aria label

--- components/srolltoTop.jsx
+++ components/srolltoTop.jsx
@@ -1,42 +1,42 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react'
-import { IoCaretUp } from 'react-icons/io5'
+import { useEffect, useState } from "react";
+import { IoCaretUp } from "react-icons/io5";
 
 export default function ScrollToTop() {
-  const [active, setIsActive] = useState(false)
+  const [active, setIsActive] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > 150) {
-        setIsActive(true)
+        setIsActive(true);
       } else {
-        setIsActive(false)
+        setIsActive(false);
       }
-    }
+    };
 
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener("scroll", handleScroll);
 
-    return () => window.addEventListener('scroll', handleScroll)
-  }, [])
+    return () => window.addEventListener("scroll", handleScroll);
+  }, []);
 
   const scrollingToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
-    })
-  }
+      behavior: "smooth",
+    });
+  };
 
   return (
     <button
       onClick={scrollingToTop}
       type="button"
-
+      aria-label="Vissza az oldal tetejére."
       className={`${
-        active ? 'translate-x-0' : 'translate-x-[150px]'
+        active ? "translate-x-0" : "translate-x-[150px]"
       } fixed right-[10px] bottom-[10px] p-[5px] z-[99] bg-arany rounded-full items-center justify-center shadow-[2px_2px_15px_2px_rgba(0,0,0,0.3)] cursor-pointer hover:scale-110 transition-transform`}
     >
-      <IoCaretUp size={30} fill={'white'} />
+      <IoCaretUp size={30} fill={"white"} />
     </button>
-  )
+  );
 }
