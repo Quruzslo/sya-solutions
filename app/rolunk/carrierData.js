b6e568eceb8d4838a4c04855bf5cfe343282f447
Gál Áron Szili <sziligalaron@gmail.com>
2026-07-31 15:02:58 +0200

    karrierből kivéve 2

--- app/rolunk/carrierData.js
+++ app/rolunk/carrierData.js
@@ -44,6 +44,8 @@ export const carrierData = [
       "Legalább minimális tapasztalat Word és Excel terén",
       "Barátságos kommunikáció",
       "Csapatjáték",
+      "Telefonos kapcsolattartás és időpont-egyeztetés meglévő ügyfelekkel",
+      "Adminisztrációs támogatás az értékesítési folyamatokban",
     ],
     elonyok: [
       "Érdekel a pénzügy",
@@ -83,7 +85,7 @@ export const carrierData = [
   },
   {
     id: 3,
-    title: "Pénzügyi Munkatárs",
+    title: "Fiókvezető",
     icon: (
       <svg
         fill="#3f4603"
@@ -92,75 +94,59 @@ export const carrierData = [
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
       >
-        <path d="M16 6l2.29 2.29-4.88 4.88-4-4L2 16.59 3.41 18l6-6 4 4 6.3-6.29L22 12V6h-6z"></path>
+        <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"></path>
       </svg>
     ),
     description:
-      "Bővülő csapatunkba keresünk új kollégákat! Ha fontos számodra a rugalmas időbeosztás, a teljesítményarányos bérezés és szeretnél segíteni másoknak az álmaik megvalósításában, köztünk a helyed.",
+      "Ha szeretsz emberekkel foglalkozni, motiválni egy csapatot és fontos számodra a szakmai fejlődés, nálunk a helyed!",
     elvarasok: [
-      "Felsőfokú tanulmányok (folyamatban lévő vagy befejezett) vagy MNB Biztosításközvetítői végzettség",
-      "Heti legalább 15-20 óra munkára fordítható szabadidő",
-      "Haladó szintű számítógépes ismeretek",
-      "Aktív angol nyelvtudás",
-      "Általános érdeklődés a pénzügyek iránt",
-      "Proaktív, önálló munkavégzés és megbízhatóság",
-      "Határidők pontos betartása és diszkréció",
-      "Stílusos megjelenés és igényesség",
+      "Kiváló kommunikációs és vezetői készség",
+      "Magabiztos, határozott fellépés",
+      "Jó szervező- és problémamegoldó képesség",
+      "Önálló, felelősségteljes munkavégzés",
+      "Csapat motiválása és támogatása",
+      "Célorientált gondolkodás",
+      "Diploma, vagy pénzügyi szakképzettség",
     ],
     elonyok: [
-      "Többre hivatottság érzése és a tanulás szeretete",
-      "Segítőkész, ügyfélközpontú hozzáállás",
-      "Nyitottság a teljesítményalapú bérezésre",
+      "Van vezetői vagy csapatkoordinációs tapasztalatod",
+      "Érdekel a pénzügyi piac és az öngondoskodás",
+      "Könnyen teremtesz kapcsolatot új emberekkel",
+      "Szeretsz mentorálni és fejleszteni másokat",
+      "Nem okoz gondot a döntéshozatal és a felelősségvállalás",
     ],
   },
   {
     id: 4,
-    title: "Értékesítési Asszisztens",
+    title: "Gyakornok",
     icon: (
       <svg
         fill="#3f4603"
-        version="1.1"
-        id="Capa_1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
         width="25px"
         height="25px"
-        viewBox="0 0 31.361 31.361"
-        xmlSpace="preserve"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
       >
-        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-        <g
-          id="SVGRepo_tracerCarrier"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        ></g>
-        <g id="SVGRepo_iconCarrier">
-          <g>
-            <path d="M22.277,18.415c-3.574,0-6.474,2.897-6.474,6.474c0,3.574,2.899,6.473,6.474,6.473s6.475-2.897,6.475-6.473 C28.752,21.312,25.853,18.415,22.277,18.415z M20.889,28.869l-3.457-3.456l1.357-1.358l2.1,2.1l4.568-4.568l1.356,1.357 L20.889,28.869z"></path>
-            <path d="M4.326,28.289V1.719h13.363v6.884h6.885v9.22c0.612,0.198,1.188,0.478,1.719,0.819V7.891L18.401,0H2.609v30.008h14.293 c-0.486-0.51-0.9-1.087-1.227-1.719H4.326z"></path>
-            <circle cx="8.788" cy="11.686" r="2.426"></circle>
-            <rect x="13.391" y="10.246" width="8.238" height="2.88"></rect>
-            <circle cx="8.788" cy="18.094" r="2.426"></circle>
-            <path d="M21.629,17.488v-0.834h-8.238v2.88h3.744C18.324,18.391,19.891,17.639,21.629,17.488z"></path>
-            <circle cx="8.788" cy="24.19" r="2.426"></circle>
-            <path d="M13.391,22.751v2.88h1.49c-0.024-0.245-0.038-0.492-0.038-0.742c0-0.744,0.113-1.459,0.316-2.138H13.391z"></path>
-          </g>
-        </g>
+        <path d="M5 13.18v4L12 21l7-3.82v-4L12 17l-7-3.82zM12 3L1 9l11 6 9-4.91V17h2V9L12 3z"></path>
       </svg>
     ),
     description:
-      "Pénzügyi és biztosítási területen működő cégünk értékesítési asszisztens munkatársat keres ügyfélgondozásra, telefonos kapcsolattartásra és adminisztrációs feladatok ellátására.",
+      "Szeretnél értékes tapasztalatot szerezni egy támogató csapatban? Nálunk betekintést nyerhetsz a pénzügyi világ működésébe!",
     elvarasok: [
-      "Magabiztos, udvarias kommunikáció telefonon",
-      "Precizitás, pontosság és megbízhatóság",
-      "Jó szervezőkészség",
-      "Telefonos kapcsolattartás és időpont-egyeztetés meglévő ügyfelekkel",
-      "Adminisztrációs támogatás az értékesítési folyamatokban",
+      "Nyitottság a tanulásra",
+      "Jó kommunikáció",
+      "Precíz, igényes munkavégzés",
+      "Megbízhatóság",
+      "Alapszintű Excel és Zoom ismeret",
+      "Pozitív hozzáállás",
+      "Minimum érettségi vagy folyamatban lévő felsőfokú tanulmányok/főállás mellett",
     ],
     elonyok: [
-      "Biztosítási és pénzügyi szektorban szerzett tapasztalat",
-      "Ügyfélközpontú gondolkodásmód",
-      "Rugalmas munkavégzésre való nyitottság (Budapest / Home Office)",
+      "Érdekel a pénzügy és a gazdaság",
+      "Szeretsz emberekkel dolgozni",
+      "Nyitott vagy az új kihívásokra",
+      "Van ügyfélszolgálati vagy értékesítési tapasztalatod (nem feltétel)",
+      "Szeretnél hosszú távon fejlődni",
     ],
   },
 ];
